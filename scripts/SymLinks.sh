2565d0ec5b5cc5ea02776877ee526b4a226a6368
Donagh <xiddler@gmail.com>
2020-04-13 13:43:00 +0100

    deleted SymLinks.txt and make SymLinks.sh in scripts

---
+++ scripts/SymLinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -s $HOME/PORTABLE_ENV/bashrc /home/donagh/.bashrc 
+ln -s /home/donagh/sd64/1donaghs-stuff/personal/journal /home/donagh/journal 
+ln -s /home/donagh/sd64/2programming_repository/python /home/donagh/pyython 
+ln -s $HOME/PORTABLE_ENV/scripts /home/donagh/.scripts 
+ln -s /run/media/donagh/c60cbdfc-37a8-4e08-b2dd-6286d16beb3d/SD35-BACKUP /home/donagh/sd64 
+ln -s $HOME/PORTABLE_ENV/TODO /home/donagh/TODO 
+ln -s $HOME/PORTABLE_ENV/vimrc /home/donagh/.vimrc 
+ln -s /run/media/donagh/c60cbdfc-37a8-4e08-b2dd-6286d16beb3d/SD35-BACKUP/zim /home/donagh/ziim 
+ln -s $HOME/PORTABLE_ENV/zsh_aliases /home/donagh/.zsh_aliases 
+ln -s $HOME/PORTABLE_ENV/zshrc /home/donagh/.zshrc.pre-oh-my-zsh
+ 
