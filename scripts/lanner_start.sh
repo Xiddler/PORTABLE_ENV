c40bfb914f1ccc784bc6cac9d4386aba89c64b1d
Donagh <xiddler@gmail.com>
2020-08-24 14:47:22 +0100

    occasional commit

---
+++ scripts/lanner_start.sh
@@ -0,0 +1,11 @@
+#!/usr/sbin/zsh
+
+# activate flask virtualenv
+echo "activating the flask virtualenv..."
+# source /home/donagh/.virtualenvs/flask/bin/activate
+
+sleep 2
+
+# run the program run.py
+python /run/media/donagh/c60cbdfc-37a8-4e08-b2dd-6286d16beb3d/SD35-BACKUP/2programming_repository/python/1python_projects/lanner/lanner/run.py
+
