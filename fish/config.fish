71b46d48364e2ae45ce9fbe737e339b67e8a6a7b
Donagh <xiddler@gmail.com>
2020-10-01 20:29:41 +0100

    mainly fish and fish configs

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# the global value takes precedence over the universal value - to avoid this problem
+# -g or --global causes the specified shell variable to be given a global scope.
+# 
+set  -gx EDITOR /usr/bin/vim
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+
