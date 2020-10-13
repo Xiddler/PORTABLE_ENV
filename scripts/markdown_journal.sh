934d4df89e4e44158be6242560243b510c2a6935
Donagh <xiddler@gmail.com>
2020-10-13 12:57:56 +0100

    from RPi: edited script markdown_journal.sh and init.el

--- scripts/markdown_journal.sh
+++ scripts/markdown_journal.sh
@@ -10,7 +10,9 @@ md_journal() {
         local year=`date +%Y`
         local month=`date +%m`
         local today=`date +%Y-%m-%d`
-        local myDir='/home/donagh/sd64/1donaghs-stuff/personal/journal/'$year/$month
+        # local myDir='/home/donagh/sd64/1donaghs-stuff/personal/journal/'$year/$month
+        local
+        myDir='/home/donagh/Donaghs_Tmp/journal/'$year/$month
         mkdir -p $myDir
         local filen=$myDir/`date +%d-%m`\.md
         if [ ! -f $filen ]; 
