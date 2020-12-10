0ce8151fe20cd3f0218ea14fa8482ecf95eafeb5
Donagh <xiddler@gmail.com>
2020-12-10 15:16:28 +0000

    laptop: some autokey; small

--- scripts/markdown_journal.sh
+++ scripts/markdown_journal.sh
@@ -12,7 +12,8 @@ md_journal() {
         local today=`date +%Y-%m-%d`
         # local myDir='/home/donagh/sd64/1donaghs-stuff/personal/journal/'$year/$month
         local
-        myDir='/home/donaghm/Donaghs_Tmp/journal/'$year/$month
+        # myDir='/home/donaghm/Donaghs_Tmp/journal/'$year/$month
+        myDir='/home/donaghm/SD64/DONAGHS/journal/'$year/$month
         mkdir -p $myDir
         local filen=$myDir/`date +%d-%m`\.md
         if [ ! -f $filen ]; 
