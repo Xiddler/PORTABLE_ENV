3c84b14205fa3ddf1b50c20094ab02778c5a6bcc
Donagh <xiddler@gmail.com>
2021-04-29 18:56:59 +0100

    cheats etc

---
+++ MARKS/mark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# export CDPATH=.:~/.marks/
+
+# function mark {
+  # ln -sr "$(pwd)" ~/.marks/"$1"
+# }
+
+# mark @name      # create a bookmark
+# cd @name        # jump to bookmark
+# cd @<tab>       # list bookmarks
+# cd @n<tab>      # auto-complete
+# cd @name/<tab>  # can access sub-directories within bookmarks
+# export MARKPATH=$HOME/.marks
+export MARKPATH=$HOME/PORTABLE_ENV/MARKS/marks
+
+mark() {
+  mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+}
+
+unmark() {
+  rm -i "$MARKPATH/$1"
+}
+
+marks() {
+  ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+}
+
+jump() {
+  cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+}
