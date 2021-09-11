2fa637884371849b1928dfa8d8b948a5c31281e1
Donagh <xiddler@gmail.com>
2021-09-11 12:18:17 +0100

    cheats, scripts, aliases

---
+++ scripts/simple_backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+tar -czf /home/donaghm/REPOS/TESTING/dot_files.tar.gz /home/donaghm/REPOS/TESTING/dot_files/
+
+echo "test backup completed"
+echo "dot_files directory backed up to TESTING/dot_files.tar.gz"
