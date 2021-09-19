5ad01161acfaa775de827114f5c3b7b9a9cff274
Donagh <xiddler@gmail.com>
2021-09-19 10:01:39 +0100

    after popping a temporary stash

--- scripts/simple_backup.sh
+++ scripts/simple_backup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-tar -czf /home/donaghm/REPOS/TESTING/dot_files.tar.gz /home/donaghm/REPOS/TESTING/dot_files/
+# tar -czf /home/donaghm/REPOS/TESTING/dot_files.tar.gz /home/donaghm/REPOS/TESTING/dot_files/
 
-echo "test backup completed"
-echo "dot_files directory backed up to TESTING/dot_files.tar.gz"
+# echo "test backup completed"
+# echo "dot_files directory backed up to TESTING/dot_files.tar.gz"
+
+
+# now=$("/usr/sbin/date +%Y-%m-%d_%H:%M:%S")
+now=$(date "+%Y-%m-%d_%H:%M:%S")
+
+# exec /usr/sbin/touch "/tmp/file_$now"
+/usr/sbin/touch "/tmp/file_$now"
+# /usr/sbin/touch "/tmp/file_123"
+# exec /usr/sbin/touch "/tmp/file_$now"
