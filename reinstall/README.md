7de3cd5326a82fbd4e99d748fa64ecc6a15001ba
Donagh <xiddler@gmail.com>
2021-12-22 21:09:35 +0000

    reinstall/ added. vim-commentary added. scripts for backup and rsync added. cheats

---
+++ reinstall/README.md
@@ -0,0 +1,18 @@
+Opened 2021-12-22
+
+This is a helper file when dealing with a full reinstall like I did in Dec 2021.
+
+I had a timeshift snapshot but for some reason the kernel and the boot process did not see eye-to-eye.
+
+The file /run/media/donagh/01d4c077-4709-4b5b-9431-087bc9060d68/full_manjaro_reinstall_Dec2021.md has notes I took while reinstalling
+manjaro over the course of almost 2 days.
+
+I used the following command to store this file PORTABLE_ENV
+rsync -a ~/SD64/full_manjaro_reinstall_Dec2021.md .         NOTE: that . is the current folder!
+NOTE: maybe add this file to .gitignore. GOOD IDEA.
+
+
+
+The only glitch I could not easily fix was caffeine-ng. 
+
+END - 2021-12-22_21:00
---
+++ scripts/rsync_backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# see cheat rsync or man rsync for help
+
+# this script backs up the files and folders of value to 
+# ~/BACKUPS/RSYNCED  which is symlinked to ~/cifs_share/RSYNCED linked to the RPi
+
+
+# source directory on laptop is
+#$HOME/BACKUPS/RSYNCED               (symlinked to ~/cifs_share/RSYNCED)
+
+# destination is RPi/usb_128/RSYNCED
+
+# zim_ORG
+sudo rsync -a --delete ~/DONAGHS/MY_ZIM/ORG ~/BACKUPS/RSYNCED/zim_ORG && \
+sudo rsync -a --delete ~/DONAGHS/MY_ZIM/ORG.txt ~/BACKUPS/RSYNCED/zim_ORG
+
+# zim_Computer
+sudo rsync -a --delete ~/DONAGHS/MY_ZIM/Computer ~/BACKUPS/RSYNCED/zim_Computer && \
+sudo rsync -a --delete ~/DONAGHS/MY_ZIM/Computer.txt ~/BACKUPS/RSYNCED/zim_Computer
+
+# personal
+sudo rsync -a --delete ~/DONAGHS/personal/ ~/BACKUPS/RSYNCED/personal/
+
+# all_orgmode
+sudo rsync -a --delete  ~/Dropbox/org-mode/org ~/BACKUPS/RSYNCED/all_org/
+
+
