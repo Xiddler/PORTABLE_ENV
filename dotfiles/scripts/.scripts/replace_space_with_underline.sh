e5cd408f54f0d9c173f76acb28c68219d706fb6b
Donagh <xiddler@gmail.com>
2022-10-14 23:31:35 +0100

    dotfile/scripts/replace_space_with_underline

---
+++ dotfiles/scripts/.scripts/replace_space_with_underline.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+set -e
+
+echo "This script will replace a single space with a _ for use when making filenames in Linux"
+
+echo  "Paste in the item..." 
+
+read  m
+
+echo " "
+
+echo "$m will now have it's spaces replaced with _ and is in the
+clipboard ready for pasting. What more do you want?"
+
+echo " "
+
+echo "$m" | sed -e 's/\s/_/g' | xclip -selection clipboard
+
+
+
